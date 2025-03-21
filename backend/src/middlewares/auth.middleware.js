b1f21207d1cdd608cb6e95443499cd0337e7faf5
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-03-21 22:34:26 +0530

    Create Page Ui and functionality

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import User from '../models/user.model.js'
 const ProtectRoute = async (req, res, next) => {
     try {
         // get Token 
-        const token = req.headers("Authorization").replace("Bearer ", "");
+        const token = req.header("Authorization").replace("Bearer", "");
         if (!token) {
             return res.status(401).json({ message: "No authentication token, access denied" });
         }
